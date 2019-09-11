b0c14b62283ce27262c42045fdf6985f0dd7eefe
Jakob Scholz <jakob@scholzundscholz.net>
2019-09-11 07:50:03 +0200

    Working again after refactoring

--- app/models/shared/does_entry.rb
+++ app/models/shared/does_entry.rb
@@ -4,10 +4,5 @@ module Shared::DoesEntry
     belongs_to domain, optional: true
 
     validates :creator, :name, presence: true
-    accepts_nested_attributes_for domain, reject_if: :reject_domain
-
-    def reject_domain(params)
-      drawer_id != 0
-    end
   end
 end
