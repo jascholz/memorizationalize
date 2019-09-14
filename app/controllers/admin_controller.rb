ee4f8dbc5f392e82ee7482e2771732631e1beac3
Jakob Scholz <jakob@scholzundscholz.net>
2019-09-14 16:09:45 +0200

    Secure admin controller from non-admin users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
 
+  before_action :require_admin
+
   def index
     load_klasses
   end
@@ -9,6 +11,10 @@ class AdminController < ApplicationController
   def load_klasses
     Rails.application.eager_load!
     @klasses = ApplicationRecord.descendants.collect(&:name).compact.collect(&:constantize)
+  end
+
+  def require_admin
+    redirect_to root_path if current_user.role != 'admin'
   end
 
 end
