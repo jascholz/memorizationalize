4a915b3a68b6d77363781c87b00324df7ad9df52
Jakob Scholz <jakob@scholzundscholz.net>
2019-09-15 08:46:11 +0200

    Better header link behavior

--- app/assets/javascripts/compilers/header.js
+++ app/assets/javascripts/compilers/header.js
@@ -2,8 +2,10 @@ up.compiler('.header', (header) => {
   let body = document.querySelector('body')
   let title = header.querySelector('.header--title')
   up.on(title, 'click', (event) => {
-    if (body.getAttribute('layout') == 'application') {
-      event.preventDefault()
+    event.preventDefault()
+    if (body.getAttribute('layout') !== 'application') {
+      up.visit('/', { target: 'body' })
+    } else {
       up.scroll(document.scrollingElement, 0)
       up.emit('app:reset')
     }
