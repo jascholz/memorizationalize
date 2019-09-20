2722ce22cf81e98002ea4cfd3f4b6138badcbd54
Jakob Scholz <jakob@scholzundscholz.net>
2019-09-20 08:27:40 +0200

    Expire old invitations once a day

--- app/models/drawer/invitation.rb
+++ app/models/drawer/invitation.rb
@@ -9,6 +9,13 @@ class Drawer::Invitation < ActiveType::Object
 
   validates :invite_code, :drawer, presence: true
 
+  def self.expire_invitations
+    drawers = Drawer.where('invite_code IS NOT NULL').where('invitation_expiry < ?', Date.today)
+    drawers.each do |drawer|
+      drawer.update(invite_code: nil, invitation_expiry: nil)
+    end
+  end
+
   private
 
   def find_drawer
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
-every 1.day, at: '2:30 am' do
+every 1.day, at: '2:12 am' do
   runner 'User::Runner.delete_unconfirmed_users'
+end
+
+every 1.day, at: '2:15 am' do
+  runner 'Drawer::Invitation.expire_invitations'
 end
